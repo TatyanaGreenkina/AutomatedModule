4a677f5c569a526eca54cf4d316a363315e90493
TatyanaGreenkina <tgreenkina@gmail.com>
2018-11-06 12:02:02 +0300

    Modified module to work with embeddings

--- embeddings.py
+++ embeddings.py
@@ -1,10 +1,11 @@
 from scipy import spatial
+import re
 
 
 class Embedding(object):
     def __init__(self):
         self.emb_dict = {}
-
+        self.answers = {}
         with open('./matrix/mentadata_sent.txt', 'r', encoding='utf8') as f:
             name_emb = [row.replace('\n', '') for row in f.readlines()]
 
@@ -16,15 +17,25 @@ class Embedding(object):
                          for num in embed[i].replace('\n', '').split('\t')[1:]]
             self.emb_dict[name_emb[i]] = row_embed
 
-    def search_words(self, word):
-        answers = {}
-        words = {}
-        vec1 = self.emb_dict.get(word)
+    def search_key(self, word):
+        word_set = [i for i in re.sub(r'[\W\d]', ' ', word).split(' ') if len(i) > 1]
         for key in self.emb_dict.keys():
-            vec2 = self.emb_dict.get(key)
-            similarity = 1 - spatial.distance.cosine(vec1, vec2)
-            if 0 < similarity < 1.0e-02:
-                answers[similarity] = key
-        for i in sorted(answers.keys()):
-            words[answers[i]] = i
-        return words
+            key_set = [i for i in re.sub(r'[\W\d]', ' ', key).split(' ') if len(i) > 1]
+            if set(word_set).issubset(set(key_set)):
+                return self.emb_dict[key]
+
+    def cos(self, word, label):
+        vec1 = self.search_key(word)
+        vec2 = self.search_key(label)
+        return spatial.distance.cosine(vec1, vec2)
+
+    def search_words(self, word):
+        for key in self.emb_dict.keys():
+            similarity = self.cos(word, key)
+            self.answers[key] = similarity
+        self.answers = {word: rang for word, rang in sorted(self.answers.items(), key=lambda item: item[1])}
+        return self.answers
+
+    def rang(self, word, label):
+        words = {word: rang for rang, word in enumerate(self.search_words(word).keys())}
+        return words[label]
